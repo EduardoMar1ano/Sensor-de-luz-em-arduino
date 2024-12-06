af0c8fce3129912454df37ce201657dd74f6a4b5
Eduardo Mariano <eduardomariano1810@gmail.com>
2024-12-06 18:57:36 -0300

    Add files via upload

---
+++ projeto_sensor_de_luz1.ino
@@ -0,0 +1,16 @@
+ // Projeto Sensor de luz
+ int piezoPin = 8; 
+ int ldrPin = 0;  
+
+ int ldrValue = 0; 
+ void setup() {
+ 
+ }
+ void loop() {
+ ldrValue = analogRead(ldrPin);  
+tone(piezoPin,1000);       
+delay(25);           
+noTone(piezoPin);        
+delay(ldrValue);         
+}
+ 
